f91cc0c0b052aaafbf9ed4e8663aa0f5f7f2ed88
namil7657 <164168956+namil7657@users.noreply.github.com>
2024-03-24 23:37:35 +0900

    Create holy.c
    C 보고서

---
+++ holy.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+  int main(){
+  printf("Hello World!");
+  int a,b;
+  scanf("%d %d", &a, &b);   
+  printf("%d",a+b);
+  printf("%d",a-b);
+  printf("%d",a*b);
+  printf("%d",a/b);
+  printf("%d",a%b);
+}
